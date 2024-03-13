0047d1bfbdd3a4ed4fd4141e4dceb49b0b1c53f3
tolunay.aktas <tolunayaktas@gmail.com>
2024-03-13 16:23:58 +0100

    Increase/decrease/remove buttons on the Store page have been dynamized.

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
 import { formatCurrency } from "../utils/formatCurrency"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 
 type StoreItemProps = {
     id: number
@@ -10,8 +11,10 @@ type StoreItemProps = {
 
 export function StoreItem({ id, name, price, imgUrl }: 
 StoreItemProps) {
-  const quantity = 0
-  return <Card>
+  const { getItemQuantity, increaseItemQuantity, decreaseItemQuantity, removeFromCart} = useShoppingCart()
+  const quantity = getItemQuantity(id)
+  return (
+  <Card className="h-100">
      <Card.Img 
      variant="top" 
      src={imgUrl} 
@@ -23,6 +26,27 @@ StoreItemProps) {
         <span className="fs-2">{name}</span>
         <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+           {quantity === 0 ? (
+            <Button className="w-100" onClick={() => increaseItemQuantity(id)}>+ Add to Cart</Button>
+           ) : ( <div className="d-flex align-items-center flex-column" 
+           style={{ gap: ".5rem"}}
+           >
+            <div 
+            className="d-flex align-items-center justify-content-center"
+            style={{ gap: ".5rem"}}
+            >
+               <Button onClick={() => decreaseItemQuantity(id)}>-</Button>
+               <div>
+               <span className="fs-5">{quantity}</span>in cart
+               </div>
+               <Button onClick={() => increaseItemQuantity(id)}>+</Button>
+               </div>
+               <Button onClick={() => removeFromCart(id)} variant="danger" size="sm">Remove</Button>
+            </div>
+            )}
+        </div>
      </Card.Body>
   </Card>
+  ) 
 }
